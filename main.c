9e7af6d5249caa73e9b71bebe095c3f5aaceeceb
el-cs <eliang50@stuy.edu>
2024-11-26 14:37:20 -0500

    Finish lab

--- main.c
+++ main.c
@@ -5,41 +5,38 @@
 #include <time.h>
 #include <fcntl.h>
 
-static int time = 0;
+void childSleep() {
+    unsigned char n;
+    int file = open("/dev/urandom", O_RDONLY, 0);
+    read(file, &n, 1);
+    close(file);
+    n = (n & 0b11) + 1;
+    printf("%d %dsec\n", getpid(), n);
+    sleep(n);
+    printf("%d finished after %dsec\n", getpid(), n);
+    exit(n);
+}
 
 int main(int argc, char const *argv[]) {
-  printf("%d about to create 2 child processing\n", getpid());
+  printf("%d about to create 2 child processes\n", getpid());
   pid_t p;
   p = fork();
   if (p < 0) {
     perror("fork fail");
     exit(1);
   } else if (p == 0) {
-    unsigned int n;
-    int file = open("/dev/urandom", O_RDONLY, 0);
-    read(file, &n, 4);
-    close(file);
-    n %= 5;
-    printf("%d %dsec\n", getpid(), n);
-    sleep(n);
-    printf("%d finished after %dsec\n", getpid(), n);
+    childSleep();
   } else {
-    printf("parent\n");
     p = fork();
     if (p < 0) {
       perror("fork fail");
       exit(1);
     } else if (p == 0) {
-      unsigned int n;
-      int file = open("/dev/urandom", O_RDONLY, 0);
-      read(file, &n, 4);
-      close(file);
-      n %= 5;
-      printf("%d %dsec\n", getpid(), n);
-      sleep(n);
-      printf("%d finished after %dsec\n", getpid(), n);
+      childSleep();
     } else {
-      printf("parent still\n");
+      int status = 0;
+      pid_t child = waitpid(-1, &status, 0);
+      printf("Main Process %d is done. Child %d slept for %dsec\n", getpid(), child, WEXITSTATUS(status));
     }
   }
   return 0;
